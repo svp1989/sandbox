e8173acc6f94992321c8bb278cc6d7a2a52954e4
skvortsov <vskvortsov@amulex.ru>
2017-07-10 17:06:45 +0300

    add js tableToExcel

---
+++ js/htmlToExcel.js
@@ -0,0 +1,41 @@
+/**
+ * класс создания excel из html table
+ * @type object
+ *         tableId: string, - id таблицы из которой берем данные
+ *         fileName: string, - имя под которым файл будет загружен
+ *         download: download, - загружает файл
+ *         createElement: createElement   - возвращает элемент для загрузки файла
+ */
+var htmlToExcel = {
+
+    tableId: '',
+    fileName: '',
+    charset: 'UTF-8',
+
+    //Загружаем файл
+    download: function (tableId, fileName) {
+        this.fileName = fileName;
+        this.tableId = tableId;
+        this.createElement().click();
+    },
+
+    //Создаем элемент
+    createElement: function () {
+        var a = document.createElement('a'),
+            dataType = 'data:application/vnd.ms-excel;base64,',
+            tableHtml = '<html><head><meta http-equiv="content-type" content="text/html; charset='+charset+'" /></head>' +
+                '<body>' + document.getElementById(this.tableId).outerHTML + '</body>' +
+                '</html>';
+
+        a.href = dataType + this.base64(tableHtml);
+        a.download = this.fileName;
+
+        return a;
+    },
+
+    //base64
+    base64: function (s) {
+        return window.btoa(unescape(encodeURIComponent(s)));
+    }
+
+};
